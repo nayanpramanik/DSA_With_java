8756efd5fa89e125ddd650f3b7218fd1072eaa7c
Nayan Kumar Pramanik <npramanik121@gmail.com>
2024-08-11 23:02:36 +0530

    upadet base
    recursion new code of DSA

---
+++ recursion/SumOfSries.java
@@ -0,0 +1,15 @@
+public class SumOfSries {
+    
+    static int SumSeries(int n){
+        if(n == 0 || n ==1){
+            return n;
+        }
+        int ans = SumSeries(n-1) + n;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(SumSeries(n));
+    }
+}
