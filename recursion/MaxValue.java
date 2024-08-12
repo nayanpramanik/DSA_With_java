4578dba61f5ee8b16d80bbd066ebd21b3507c8ee
Nayan Kumar Pramanik <npramanik121@gmail.com>
2024-08-12 22:27:25 +0530

    update
    Recursion & String(Basic)

---
+++ recursion/MaxValue.java
@@ -0,0 +1,17 @@
+public class MaxValue {
+    static int MaxArray(int [] arr, int idx){
+        //        base case
+                if(idx == arr.length-1){
+                    return arr[idx];
+                }
+                int smallAns = MaxArray(arr,idx + 1);
+        
+                return Math.max(arr[idx],smallAns);
+            }
+        
+            public static void main(String[] args) {
+                int arr[]= {15,13,3,10,9,12,2};
+                int idx = 0;
+                System.out.println(MaxArray(arr,idx));
+}
+}
